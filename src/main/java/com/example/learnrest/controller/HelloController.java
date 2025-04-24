5a7d67710cb435498860f34ba939328985010fa9
rudi <rstudio019@gmail.com>
2025-04-24 21:38:14 +0700

    init project with hello world controller

---
+++ src/main/java/com/example/learnrest/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.learnrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  
+  @GetMapping("/hello")
+  public String sayHello() {
+      return "Hello from Spring!";
+  }
+
+  @PostMapping("/hello")
+  public String sayHelloPost() {
+      return "Hello from Spring! (POST)";
+  }
+}
